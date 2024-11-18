9659ee2ea5c087158577eb5cda65aa44ea92158d
YUMNANASIR01 <yumnanasir719@gmail.com>
2024-11-18 00:26:26 -0800

    first

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import CountDownTimer from '@/components/CountDownTimer'
+import React from 'react'
+
+function page() {
+  return (
+    <>
+     <div className='id1 h-[100vh] flex justify-center items-center text-[black]'> 
+      <CountDownTimer/></div>
+    </>
+  )
+}
+
+export default page
